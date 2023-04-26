fcc38c9fadab619c0a000437cb59483c3ab734d1
admiration91 <almaslov91@gmail.com>
2023-04-26 15:09:17 +0300

    admin changes

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('api/docs/', TemplateView.as_view(template_name='redoc.html')),
     path('api/', include('users.urls')),
     path('api/', include('api.urls')),
